bd86a58054412fdd7c28f88e4f9645c0c32ab724
Adam Wright <144672422+adamwright103@users.noreply.github.com>
2025-01-24 17:31:39 +0000

    lab0 complete

---
+++ lab-zero/lab0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * Revise lab0.c. Use a while loop to calculate the sum of positive even integers below 200 and print the
+ * result to the standard output. This is very similar to the sum example we have studied. Think about how
+ * much you should increase the loop control variable to get to the next even number.
+ */
+
+int main(void)
+{
+    int sum = 0, i = 0;
+    while (i < 200) {
+        sum += i;
+        i += 2;
+    }
+    printf("Hello, World!\n");
+    printf("sum: %d\n", sum);
+    return 0;
+}
